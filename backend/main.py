c45eaacb337bcc8ad1ed8ce989361bf0d16fc460
daggumatiakhil11-dot <daggumatiakhil11@gmail.com>
2026-05-11 04:25:30 -0500

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -12,12 +12,13 @@ load_dotenv()
 # FastAPI app
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
         "https://maaad-brains.vercel.app",
-        "*"
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
